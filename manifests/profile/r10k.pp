aadbbdb831a6ba9ed660a7498453b1c85882eabf
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2024-01-17 17:27:21 -0600

    fix r10k for puppet 8

--- manifests/profile/r10k.pp
+++ manifests/profile/r10k.pp
@@ -57,14 +57,59 @@ class boss::profile::r10k (
     }
   }
 
-  package { 'gem-rugged':
+  # pin older gems for ruby 2.7
+  if $facts['puppetversion'] =~ /^7/ {
+    package { 'gem-faraday':
+      name     => 'faraday',
+      ensure   => '2.8.1',
+      provider => 'puppet_gem',
+    }
+    -> package { 'gem-faraday-net_http':
+      name     => 'faraday-net_http',
+      ensure   => '3.0.2',
+      provider => 'puppet_gem',
+      before   => Package['gem-r10k'],
+    }
+  }
+
+  # all of this for r10k ssh support
+  # build r10k dependencies inside aio puppet
+  archive { '/var/tmp/libssh2-1.11.0.tar.gz':
+    source        => 'https://libssh2.org/download/libssh2-1.11.0.tar.gz',
+    checksum_type => 'sha256',
+    checksum      => '3736161e41e2693324deb38c26cfdc3efe6209d634ba4258db1cecff6a5ad461',
+    cleanup       => true,
+    creates       => '/var/tmp/libssh2-1.11.0',
+    extract       => true,
+    extract_path  => '/var/tmp',
+  }
+  ~> exec { 'puppet-libssh2':
+    path        => ['/opt/puppetlabs/puppet/bin', '/usr/bin', '/bin'],
+    command     => 'mkdir -p /var/tmp/puppet-build ; cd /var/tmp/puppet-build && rm -rf * && cmake /var/tmp/libssh2-1.11.0 -DCMAKE_INSTALL_PREFIX=/opt/puppetlabs/puppet -DCMAKE_PREFIX_PATH=/opt/puppetlabs/puppet -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_RPATH=/opt/puppetlabs/puppet/lib -DENABLE_ZLIB_COMPRESSION=ON && make -j install && cd .. && rm -rf puppet-build',
+    refreshonly => true,
+    require     => Package[$rugged_build_packages],
+  }
+  -> archive { '/var/tmp/libgit2-1.7.1.tar.gz':
+    source       => 'https://github.com/libgit2/libgit2/archive/refs/tags/v1.7.1.tar.gz',
+    checksum_type => 'sha256',
+    checksum      => '17d2b292f21be3892b704dddff29327b3564f96099a1c53b00edc23160c71327',
+    cleanup      => true,
+    creates      => '/var/tmp/libgit2-1.7.1',
+    extract      => true,
+    extract_path => '/var/tmp',
+  }
+  ~> exec { 'puppet-libgit2':
+    path        => ['/opt/puppetlabs/puppet/bin', '/usr/bin', '/bin'],
+    command     => 'mkdir -p /var/tmp/puppet-build ; cd /var/tmp/puppet-build && rm -rf * && cmake /var/tmp/libgit2-1.7.1 -DCMAKE_INSTALL_PREFIX=/opt/puppetlabs/puppet -DCMAKE_PREFIX_PATH=/opt/puppetlabs/puppet -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_RPATH=/opt/puppetlabs/puppet/lib -DUSE_SSH=ON -DUSE_HTTPS=OpenSSL-Dynamic && make -j install && cd .. && rm -rf puppet-build',
+    refreshonly => true,
+  }
+  -> package { 'gem-rugged':
     name            => 'rugged',
     provider        => 'puppet_gem',
     # install_options are not appended to the provider command so we can't
     # just pass the --with-ssh argument through to make :(
     # here be dragons...
-    install_options => ['--no-document', 'rugged', '--', '--with-ssh'],
-    require         => Package[$rugged_build_packages],
+    install_options => ['--no-document', 'rugged', '--', '--with-ssh', '--use-system-libraries'],
   }
   -> package { 'gem-r10k':
     name     => 'r10k',
