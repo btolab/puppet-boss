31ece5f930a5b3d7644abd7bcbe8afff6d5e36ff
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2024-02-06 15:26:09 -0600

    disable selinux for now to work-around puppetboard failing to start

--- manifests/profile/base/os/redhat.pp
+++ manifests/profile/base/os/redhat.pp
@@ -1,3 +1,8 @@
 # base os redhat
 class boss::profile::base::os::redhat {
+  # set selinux to permissive/disabled for now
+  exec { 'selinux-enforce=0':
+    command => '/usr/bin/env echo 0 > /sys/fs/selinux/enforce',
+    onlyif  => '/usr/bin/env test `cat /sys/fs/selinux/enforce` -eq 1',
+  }
 }
