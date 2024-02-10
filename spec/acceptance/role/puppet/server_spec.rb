801aa15359f75a77c2271af9a66c590f31139b6b
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2024-02-09 23:02:05 -0600

    add contexts with tags to acceptance tests

--- spec/acceptance/role/puppet/server_spec.rb
+++ spec/acceptance/role/puppet/server_spec.rb
@@ -8,39 +8,43 @@ describe 'boss::role::puppet::server' do
       MANIFEST
     end
 
-    it 'behaves idempotently' do
-      idempotent_apply(pp)
+    context 'deploy', :deploy do
+      it 'behaves idempotently' do
+        idempotent_apply(pp)
+      end
     end
 
-    describe file('/opt/puppetlabs/server') do
-      it { is_expected.to be_directory }
-    end
+    context 'validate', :validate do
+      describe file('/opt/puppetlabs/server') do
+        it { is_expected.to be_directory }
+      end
 
-    describe service('puppetserver') do
-      it { is_expected.to be_running }
-      it { is_expected.to be_enabled }
-    end
+      describe service('puppetserver') do
+        it { is_expected.to be_running }
+        it { is_expected.to be_enabled }
+      end
 
-    describe service('puppetdb') do
-      it { is_expected.to be_running }
-      it { is_expected.to be_enabled }
-    end
+      describe service('puppetdb') do
+        it { is_expected.to be_running }
+        it { is_expected.to be_enabled }
+      end
 
-    describe service('puppetboard') do
-      it { is_expected.to be_running }
-      it { is_expected.to be_enabled }
-    end
+      describe service('puppetboard') do
+        it { is_expected.to be_running }
+        it { is_expected.to be_enabled }
+      end
 
-    describe port(8140) do
-      it { is_expected.to be_listening }
-    end
+      describe port(8140) do
+        it { is_expected.to be_listening }
+      end
 
-    describe command('/opt/puppetlabs/bin/r10k deploy environment -m --generate-types') do
-      its(:exit_status) { is_expected.to eq 0 }
-    end
+      describe command('/opt/puppetlabs/bin/r10k deploy environment -m --generate-types') do
+        its(:exit_status) { is_expected.to eq 0 }
+      end
 
-    describe command('/opt/puppetlabs/bin/puppet agent -t --detailed-exitcodes') do
-      its(:exit_status) { is_expected.to be_one_of [0,2] }
+      describe command('/opt/puppetlabs/bin/puppet agent -t --detailed-exitcodes') do
+        its(:exit_status) { is_expected.to be_one_of [0,2] }
+      end
     end
   end
 end
