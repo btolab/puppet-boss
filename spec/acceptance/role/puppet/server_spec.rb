1cf333dd4f8e71e4d4172ec72391df5fd3ff113e
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2024-02-09 22:57:08 -0600

    acceptance fail fast and ensure puppet agent works

--- spec/acceptance/role/puppet/server_spec.rb
+++ spec/acceptance/role/puppet/server_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper_acceptance'
 
-describe 'boss::role::puppet::server class' do
+describe 'boss::role::puppet::server' do
   context 'with default parameters' do
     let(:pp) do
       <<-MANIFEST
@@ -31,13 +31,16 @@ describe 'boss::role::puppet::server class' do
       it { is_expected.to be_enabled }
     end
 
+    describe port(8140) do
+      it { is_expected.to be_listening }
+    end
+
     describe command('/opt/puppetlabs/bin/r10k deploy environment -m --generate-types') do
       its(:exit_status) { is_expected.to eq 0 }
     end
 
-    # FIXME: not working on rhel litmusimages (no ss command?)
-    # describe port(8140) do
-    #   it { is_expected.to be_listening }
-    # end
+    describe command('/opt/puppetlabs/bin/puppet agent -t --detailed-exitcodes') do
+      its(:exit_status) { is_expected.to be_one_of [0,2] }
+    end
   end
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
 Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
+
+RSpec.configure do |c|
+  c.fail_fast = true
+end
